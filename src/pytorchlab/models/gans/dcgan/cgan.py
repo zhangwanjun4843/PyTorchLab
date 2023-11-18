f74052ca900f884e908618931f14421527e3330d
TaoChenyue <chenyue.Tao@qq.com>
2023-11-19 07:56:04 +0800

    feat(model): realize dcgan

---
+++ src/pytorchlab/models/gans/dcgan/cgan.py
@@ -0,0 +1,117 @@
+from typing import Any, Callable, Iterable
+
+import torch
+from lightning.pytorch import LightningModule
+from torch import nn
+from torch.optim import Optimizer
+
+from pytorchlab.utils.type_hint import ImageShape, IntList
+
+from .components import Discriminator, Generator
+
+OptimizerCallable = Callable[[Iterable], Optimizer]
+LossCallable = Callable[[Iterable], nn.Module]
+
+
+class DCGAN(LightningModule):
+    def __init__(
+        self,
+        in_shape: ImageShape,
+        latent_dim: int,
+        hidden_layers_g: IntList = [128, 64],
+        hidden_layers_d: IntList = [16, 32, 64, 128],
+        criterion: LossCallable = nn.BCELoss,
+        optimizer_g: OptimizerCallable = torch.optim.Adam,
+        optimizer_d: OptimizerCallable = torch.optim.Adam,
+    ) -> None:
+        super().__init__()
+        # do not optimize model automatically
+        self.automatic_optimization = False
+        # init model
+        self.latent_dim = latent_dim
+        self.generator: nn.Module = Generator(
+            latent_dim=latent_dim, out_shape=in_shape, hidden_layers=hidden_layers_g
+        )
+        self.discriminator: nn.Module = Discriminator(
+            in_shape=in_shape, hidden_layers=hidden_layers_d
+        )
+        self.criterion = criterion()
+        self.optimizer_g = optimizer_g
+        self.optimizer_d = optimizer_d
+
+    def forward(self, z: torch.Tensor) -> torch.Tensor:
+        return self.generator(z)
+
+    def configure_optimizers(self):
+        optimizer_g = self.optimizer_g(self.generator.parameters())
+        optimizer_d = self.optimizer_d(self.discriminator.parameters())
+        return optimizer_g, optimizer_d
+
+    def training_step(self, batch: tuple[torch.Tensor, torch.Tensor], batch_idx: int):
+        x, _ = batch
+        g_loss = self.generator_step(x.size(0))
+        d_loss = self.discriminator_step(x)
+        self.log_dict(
+            {
+                "g_loss": g_loss,
+                "d_loss": d_loss,
+            },
+            sync_dist=True,
+        )
+
+    def predict_step(
+        self, batch: tuple[torch.Tensor, torch.Tensor], batch_idx: int
+    ) -> Any:
+        ...
+
+    def generator_step(self, batch_size: int) -> torch.Tensor:
+        """generate an image that discriminator regard it as groundtruth
+
+        Args:
+            batch_size (int): size for one batch
+
+        Returns:
+            torch.Tensor: generator loss
+        """
+        # zero grad generator optimizer
+        optimizer_g: torch.optim.Optimizer = self.optimizers()[0]
+        optimizer_g.zero_grad()
+        # Sample noise
+        z = torch.randn(batch_size, self.latent_dim).to(self.device)
+        # Generate images
+        generated_imgs: torch.Tensor = self(z)
+        # ground truth result (all true)
+        valid = torch.ones(batch_size, 1).to(self.device)
+        g_loss: torch.Tensor = self.criterion(self.discriminator(generated_imgs), valid)
+        # loss backward
+        self.manual_backward(g_loss)
+        # update generator optimizer
+        optimizer_g.step()
+        return g_loss
+
+    def discriminator_step(self, x: torch.Tensor) -> torch.Tensor:
+        """discriminate whether x is ground truth or not
+
+        Args:
+            x (torch.Tensor): image tensor
+
+        Returns:
+            torch.Tensor: discriminator loss
+        """
+        batch_size = x.size(0)
+        # zero grad discriminator optimizer
+        optimizer_d: torch.optim.Optimizer = self.optimizers()[1]
+        optimizer_d.zero_grad()
+        # Sample noise
+        z = torch.randn(batch_size, self.latent_dim).to(self.device)
+        # Generate images
+        generated_imgs: torch.Tensor = self(z)
+        # ground truth/ fake result
+        real = torch.ones(batch_size, 1).to(self.device)
+        fake = torch.zeros(batch_size, 1).to(self.device)
+        real_loss = self.criterion(self.discriminator(x), real)
+        fake_loss = self.criterion(self.discriminator(generated_imgs), fake)
+        d_loss = real_loss + fake_loss
+        self.manual_backward(d_loss)
+        optimizer_d.step()
+        return d_loss
---
+++ src/pytorchlab/models/gans/dcgan/components.py
@@ -0,0 +1,152 @@
+import numpy as np
+import torch
+from torch import nn
+
+from pytorchlab.utils.type_hint import ImageShape, IntList, ModuleList
+
+
+def deconv_block(
+    in_features: int,
+    out_features: int,
+    norm: bool = True,
+) -> ModuleList:
+    """upsample image with scale 2
+
+    Args:
+        in_features (int): number of input features
+        out_features (int): number of output features
+        norm (bool, optional): batchnorm or not. Defaults to True.
+
+    Returns:
+        ModuleList: list of modules to realize linear layer
+    """
+    layers: ModuleList = [
+        nn.Upsample(scale_factor=2),
+        nn.Conv2d(in_features, out_features, 3, 1, 1),
+    ]
+    if norm:
+        layers.append(nn.BatchNorm2d(out_features))
+    layers.append(nn.ReLU(inplace=True))
+    return layers
+
+
+def conv_block(
+    in_features: int,
+    out_features: int,
+    norm: bool = True,
+) -> ModuleList:
+    """downsample image with scale 2
+
+    Args:
+        in_features (int): number of input features
+        out_features (int): number of output features
+        norm (bool, optional): batchnorm or not. Defaults to True.
+
+    Returns:
+        ModuleList: list of modules to realize linear layer
+    """
+    layers: ModuleList = [nn.Conv2d(in_features, out_features, 3, 2, 1)]
+    if norm:
+        layers.append(nn.BatchNorm2d(out_features))
+    layers.append(nn.ReLU(inplace=True))
+    return layers
+
+
+class Generator(nn.Module):
+    def __init__(
+        self,
+        latent_dim: int,
+        out_shape: ImageShape,
+        hidden_layers: IntList,
+    ):
+        """
+        generate image from latent code
+
+        Args:
+            latent_dim (int): dimension of latent code
+            out_shape (ImageShape): shape of output image
+            hidden_layers (IntList): define hidden layers.
+        """
+        super().__init__()
+        self.latent_dim = latent_dim
+
+        if len(out_shape) == 2:
+            self.channels = 1
+        else:
+            self.channels = out_shape[0]
+        self.height, self.width = out_shape[1:]
+
+        self.num_blocks = len(hidden_layers)
+
+        self.linear = nn.Linear(
+            latent_dim,
+            hidden_layers[0]
+            * (self.width // 2**self.num_blocks)
+            * (self.height // 2**self.num_blocks),
+        )
+
+        layers: ModuleList = [
+            nn.BatchNorm2d(hidden_layers[0]),
+            *deconv_block(hidden_layers[0], hidden_layers[0]),
+        ]
+        for i in range(len(hidden_layers) - 1):
+            layers.extend(deconv_block(hidden_layers[i], hidden_layers[i + 1]))
+        layers.extend([nn.Conv2d(hidden_layers[-1], self.channels, 3, 1, 1), nn.Tanh()])
+        self.net = nn.Sequential(*layers)
+
+    def forward(self, x: torch.Tensor):
+        x = self.linear(x)
+        x = x.view(
+            x.size(0),
+            self.channels,
+            self.height // 2**self.num_blocks,
+            self.width // 2**self.num_blocks,
+        )
+        x = self.net(x)
+        return x
+
+
+class Discriminator(nn.Module):
+    def __init__(
+        self,
+        in_shape: ImageShape,
+        hidden_layers: IntList,
+    ):
+        """
+        discriminate whether image is generated or groundtruth
+
+        Args:
+            in_shape (ImageShape): shape of input image
+            hidden_layers (ChannelList, optional): define hidden layers. Defaults to [128,256,512,1024].
+        """
+        super().__init__()
+        self.in_shape = in_shape
+        if len(in_shape) == 2:
+            self.channels = 1
+        else:
+            self.channels = in_shape[0]
+        self.height, self.width = in_shape[1:]
+
+        self.num_blocks = len(hidden_layers)
+
+        layers = conv_block(self.in_features, hidden_layers[0], norm=False)
+        for i in range(len(hidden_layers) - 1):
+            layers.extend(conv_block(hidden_layers[i], hidden_layers[i + 1]))
+        layers.extend([nn.Linear(hidden_layers[-1], 1), nn.Sigmoid()])
+        self.net = nn.Sequential(*layers)
+
+        self.linear = nn.Sequential(
+            nn.Linear(
+                hidden_layers[-1]
+                * (self.height // 2**self.num_blocks)
+                * (self.width // 2**self.num_blocks),
+                1,
+            ),
+            nn.Sigmoid(),
+        )
+
+    def forward(self, x: torch.Tensor):
+        x = self.net(x)
+        x = x.view(x.size(0),-1)
+        x = self.linear(x)
+        return x
