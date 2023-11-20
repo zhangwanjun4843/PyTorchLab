b54fcc7421963efa2fb97114af8109afd40a5e8a
TaoChenyue <chenyue.Tao@qq.com>
2023-11-20 15:13:11 +0800

    ✨ feat(utils): scripts
    add make_gif script and define it in pdm

---
+++ src/pytorchlab/utils/cli.py
@@ -0,0 +1,7 @@
+﻿from jsonargparse import CLI
+
+from pytorchlab.utils.vision import make_gif
+
+
+def make_gif_cli():
+    CLI(make_gif)
--- src/pytorchlab/utils/vision.py
+++ src/pytorchlab/utils/vision.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-
 from PIL import Image
 
 
@@ -9,19 +8,29 @@ def make_gif(
     suffix: str = "png",
     duration: int = 100,
     loop: int = 0,
+    start: int = 0,
+    end: int | None = None,
     step: int = 1,
 ):
-    """use image below image_dir path with suffix to make gif
+    """
+    concrate images into one gif
 
     Args:
-        save_path (str | Path): gif path to save
-        image_dir (str | Path): sourse images directory
-        suffix (str, optional): image suffix. Defaults to 'png'.
+        save_path (str | Path): path to save gif
+        image_dir (str | Path): directory path of images
+        suffix (str, optional): image suffix. Defaults to "png".
+        duration (int, optional): duration to display each image. Defaults to 100.
+        loop (int, optional): replay times when finishing. Defaults to 0.
+        start (int, optional): start index of image list. Defaults to 0.
+        end (int | None, optional): end index of image list. Defaults to None.
         step (int, optional): skip images with step. Defaults to 1.
     """
     image_list: list[Image.Image] = [
         Image.open(x) for x in Path(image_dir).glob(f"*.{suffix}")
-    ][::step]
+    ]
+    if end is None:
+        end = len(image_list)
+    image_list = image_list[start:end:step]
     image_list[0].save(
         save_path,
         format="GIF",
